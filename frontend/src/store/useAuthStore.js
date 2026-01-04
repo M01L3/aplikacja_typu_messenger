171518fc7904751ed0bbbf8ad9b839133ec493df
M01L3 <Michal1Kvv@gmail.com>
2026-01-04 17:14:32 +0100

    Project complited

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001": "/"
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -86,9 +86,22 @@ export const useAuthStore = create((set, get) => ({
   },
 
   connectSocket: () => {
+    const {authUser} = get();
+    if (!authUser || get().socket?.connected) return;
     const socket = io(BASE_URL, {
-      
+      query: {
+        userId: authUser._id,
+      }
     })
+    socket.connect();
+
+    set({ sockey:socket});
+
+    socket.on("getOnlineUsers", (userIds) =>{
+      set({ onlineUsers: userIds });
+    });
   },
-  disconnectSocket: () => {},
+  disconnectSocket: () => {
+    if(get().socket?.connected) get().socket.disconnect();
+  },
 }));
